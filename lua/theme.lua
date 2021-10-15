00c5f59dc2e1fbe54ce388c3f6da5d15bf724906
Gilberto Diaz <me@gilbertodiaz.com>
2021-10-15 08:44:45 -0500

    Change theme to light

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,9 +1,14 @@
+--[[ Material
+  url:  https://github.com/marko-cerovac/material.nvim
+]]
 require("material").setup({
   italics = {
     comments = true,
-    keywords = true
+    keywords = true,
+    functions = true
   }
 })
 lvim.colorscheme = "material"
-vim.g.material_style = "palenight"
+vim.g.material_style = "lighter"
 lvim.builtin.lualine.options.theme = "material-nvim"
+
