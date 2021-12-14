8ec7ec45e3a25d7de93e4cec95b31583bfbc095d
Gilberto Diaz <me@gilbertodiaz.com>
2021-12-13 20:01:37 -0600

    Install 1.0.0-rc from master

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,2 +1,22 @@
 lvim.builtin.telescope.pickers = { find_files = { hidden = true } } -- Show hidden files in results
 lvim.builtin.telescope.defaults = { file_ignore_patterns = { "^.git/", "node_modules/" } }
+
+-- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
+-- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
+local _, actions = pcall(require, "telescope.actions")
+lvim.builtin.telescope.defaults.mappings = {
+--   -- for input mode
+  i = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+    ["<C-n>"] = actions.cycle_history_next,
+    ["<C-p>"] = actions.cycle_history_prev,
+  },
+  -- for normal mode
+  n = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+  },
+}
+
+
---
+++ lua/user/treesitter.lua
@@ -0,0 +1,14 @@
+lvim.builtin.treesitter.ensure_installed = {
+  "go",
+  "bash",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "css",
+  "yaml",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
