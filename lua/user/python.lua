2086bcca760dd797093afba02e644753d009f003
Gilberto Diaz <me@gilbertodiaz.com>
2021-12-14 11:10:11 -0600

    Refactor for using new api

--- lua/user/python.lua
+++ lua/user/python.lua
@@ -1,2 +1,12 @@
-lvim.lang.python.formatters = { { exe = "black" } }
-lvim.lang.python.linters = { { exe = "flake8" } }
+-- lvim.lang.python.formatters = { { exe = "black" } }
+-- lvim.lang.python.linters = { { exe = "flake8" } }
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { exe = "black", filetypes = { "python"} }
+}
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  { exe = "flake8", filetypes = { "python"} }
+}
--- lua/user/theme.lua
+++ lua/user/theme.lua
@@ -1,14 +1,29 @@
 --[[ Material
   url:  https://github.com/marko-cerovac/material.nvim
 ]]
-require("material").setup({
-  italics = {
-    comments = true,
-    keywords = true,
-    functions = true
+local M = {}
+function M.setup()
+  lvim.colorscheme = "material"
+  vim.g.material_style = "palenight"
+  lvim.builtin.lualine.options.theme = "material-nvim"
+
+  require("material").setup {
+    italics = {
+      comments = true,
+      keywords = true,
+      functions = true
+    }
   }
-})
-lvim.colorscheme = "material"
-vim.g.material_style = "palenight"
-lvim.builtin.lualine.options.theme = "material-nvim"
+end
+return M
+-- require("material").setup({
+--   italics = {
+--     comments = true,
+--     keywords = true,
+--     functions = true
+--   }
+-- })
+-- lvim.colorscheme = "material"
+-- vim.g.material_style = "palenight"
+-- lvim.builtin.lualine.options.theme = "material-nvim"
 
